879d4dedbfb5c4a515379c64a84047f54bc57158
HS <562358155@qq.com>
2017-06-06 15:46:50 +0800

    my_first_python

---
+++ test6.py
@@ -0,0 +1,14 @@
+#!/user/bin/env python
+# -*- coding: utf-8 -*-
+# Author:HanShuo
+
+def move(n,a,b,c):
+    if n==1:
+        print(a,"-->",c)
+    else:
+        move(n-1,a,c,b)
+        move(1,a,b,c)
+        move(n-1,b,a,c)
+
+
+print(move(2,"a","b","c"))
