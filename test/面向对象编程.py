27dabbb269b8f80bda02567415b7b2ea1eb886d5
HS <562358155@qq.com>
2017-06-09 09:45:56 +0800

    Default

--- test/面向对象编程.py
+++ test/面向对象编程.py
@@ -1,13 +1,24 @@
 #!/user/bin/env python
 # -*- coding: utf-8 -*-
 # Author:HanShuo
-std1 = {'name': 'Michael', 'score': 98}
-std2 = {'name': 'Bob', 'score': 81}
-L=[std1,std2]
-def print_peoples(std):
-    return ('%s: %s' % (std['name'], std['score']))
+# std1 = {'name': 'Michael', 'score': 98}
+# std2 = {'name': 'Bob', 'score': 81}
+# L=[std1,std2]
+# def print_peoples(std):
+#     return ('%s: %s' % (std['name'], std['score']))
+#
+# # print(L)
+#
+# # print(print_peoples(std1))
+# print(list(i for i in L)[0])
+#面向对象
+class Student(object):
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
 
-# print(L)
+    def print_score(self):
+        print("%s : %s" % (self.name, self.score))
 
-# print(print_peoples(std1))
-print(list(i for i in L)[0])
+
+
